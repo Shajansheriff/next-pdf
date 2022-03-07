76e1bd915fec1523165d888a995f780d2f0c06cc
Shajahan <shajansheriff@gmail.com>
2022-03-08 00:56:31 +0530

    export pdf api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import generatePDF from '../../lib/pdf'
+import { renderToStaticMarkup, renderToString } from 'react-dom/server';
+import Card from '../../components/card';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const pdf = await generatePDF(`
+  <html>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.1/tailwind.min.css">
+    </head>
+    <body>
+     ${renderToString(<Card />)}
+    </body>
+  </html>
+`);
+  
+  // prompt to download pdf
+  res.setHeader('Content-disposition', 'attachment; filename="article.pdf');
+  
+  // set content type
+  res.setHeader('Content-Type', 'application/pdf');
+  
+  // output the pdf buffer
+  res.end(pdf);
 }
